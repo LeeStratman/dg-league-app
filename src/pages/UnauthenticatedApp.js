0cce1d9fc46d0a968cba924485f614eb4438dac9
Lee Stratman <stratmanlc10@gmail.com>
2021-04-19 18:08:45 -0500

    Move background color from signin and signup forms to parent component.

--- src/pages/UnauthenticatedApp.js
+++ src/pages/UnauthenticatedApp.js
@@ -1,18 +1,20 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
+import Header from "../components/Header";
 import Home from "./unauthenticated/Home";
 import Signup from "./unauthenticated/Signup";
-import Header from "../components/Header";
+import Signin from "./unauthenticated/Signin";
 
 const UnauthenticatedApp = () => {
   return (
-    <>
+    <div className="min-h-screen bg-gray-50">
       <Header />
       <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/signup" exact component={Signup} />
+        <Route path="/signin" exact component={Signin} />
       </Switch>
-    </>
+    </div>
   );
 };
 
--- src/pages/unauthenticated/Signin.js
+++ src/pages/unauthenticated/Signin.js
@@ -1,6 +1,6 @@
 import Logo from "../../components/Logo";
 
-export default function Signup() {
+export default function Signin() {
   return (
     <div className="flex flex-col justify-center py-12 sm:px-6 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
@@ -8,50 +8,13 @@ export default function Signup() {
           <Logo size={200} />
         </div>
         <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-          Sign up to create an account
+          Sign in to your account
         </h2>
       </div>
 
       <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
         <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
           <form className="space-y-6" action="#" method="POST">
-            <div className="mt-6 grid grid-cols-1 gap-y-6 gap-x-4 sm:grid-cols-6">
-              <div className="sm:col-span-3">
-                <label
-                  htmlFor="first_name"
-                  className="block text-sm font-medium text-gray-700"
-                >
-                  First name
-                </label>
-                <div className="mt-1">
-                  <input
-                    type="text"
-                    name="first_name"
-                    id="first_name"
-                    autoComplete="given-name"
-                    className="input_basic"
-                  />
-                </div>
-              </div>
-
-              <div className="sm:col-span-3">
-                <label
-                  htmlFor="last_name"
-                  className="block text-sm font-medium text-gray-700"
-                >
-                  Last name
-                </label>
-                <div className="mt-1">
-                  <input
-                    type="text"
-                    name="last_name"
-                    id="last_name"
-                    autoComplete="family-name"
-                    className="input_basic"
-                  />
-                </div>
-              </div>
-            </div>
             <div>
               <label
                 htmlFor="email"
