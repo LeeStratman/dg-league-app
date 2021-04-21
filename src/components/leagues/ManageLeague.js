8103bd6edfbf080e5584a1b28fff028c8ad0283c
Lee Stratman <stratmanlc10@gmail.com>
2021-04-21 17:18:41 -0500

    Add league name to mange league page.

--- src/components/leagues/ManageLeague.js
+++ src/components/leagues/ManageLeague.js
@@ -29,40 +29,47 @@ const ManageLeague = () => {
   ) : (
     <div>
       {league.isIdle ? null : league.data ? (
-        <div className="mt-4 lg:grid lg:grid-cols-12 lg:gap-x-5">
-          <aside className="py-6 px-2 sm:px-6 lg:py-0 lg:px-0 lg:col-span-3">
-            <nav className="space-y-1">
-              {navigation.map((item) => (
-                <a
-                  key={item.name}
-                  href={item.href}
-                  className={classNames(
-                    item.current
-                      ? "bg-gray-50 text-indigo-700 hover:text-indigo-700 hover:bg-white"
-                      : "text-gray-900 hover:text-gray-900 hover:bg-gray-50",
-                    "group rounded-md px-3 py-2 flex items-center text-sm font-medium"
-                  )}
-                  aria-current={item.current ? "page" : undefined}
-                >
-                  <item.icon
+        <>
+          <div className="mt-4 pb-5 border-b border-gray-200">
+            <h3 className="text-lg leading-6 font-medium text-gray-900">
+              {league.data.name}
+            </h3>
+          </div>
+          <div className="mt-4 lg:grid lg:grid-cols-12 lg:gap-x-5">
+            <aside className="py-6 px-2 sm:px-6 lg:py-0 lg:px-0 lg:col-span-3">
+              <nav className="space-y-1">
+                {navigation.map((item) => (
+                  <a
+                    key={item.name}
+                    href={item.href}
                     className={classNames(
                       item.current
-                        ? "text-indigo-500 group-hover:text-indigo-500"
-                        : "text-gray-400 group-hover:text-gray-500",
-                      "flex-shrink-0 -ml-1 mr-3 h-6 w-6"
+                        ? "bg-gray-50 text-indigo-700 hover:text-indigo-700 hover:bg-white"
+                        : "text-gray-900 hover:text-gray-900 hover:bg-gray-50",
+                      "group rounded-md px-3 py-2 flex items-center text-sm font-medium"
                     )}
-                    aria-hidden="true"
-                  />
-                  <span className="truncate">{item.name}</span>
-                </a>
-              ))}
-            </nav>
-          </aside>
+                    aria-current={item.current ? "page" : undefined}
+                  >
+                    <item.icon
+                      className={classNames(
+                        item.current
+                          ? "text-indigo-500 group-hover:text-indigo-500"
+                          : "text-gray-400 group-hover:text-gray-500",
+                        "flex-shrink-0 -ml-1 mr-3 h-6 w-6"
+                      )}
+                      aria-hidden="true"
+                    />
+                    <span className="truncate">{item.name}</span>
+                  </a>
+                ))}
+              </nav>
+            </aside>
 
-          <div className="space-y-6 sm:px-6 lg:px-0 lg:col-span-9">
-            <DetailsForm league={league.data} />
+            <div className="space-y-6 sm:px-6 lg:px-0 lg:col-span-9">
+              <DetailsForm league={league.data} />
+            </div>
           </div>
-        </div>
+        </>
       ) : (
         <div>League not found</div>
       )}
