185b0992fdf0afa8e23dfd7f962b91f380170078
Lee Stratman <stratmanlc10@gmail.com>
2021-04-21 15:15:07 -0500

    Add update league form.

---
+++ src/components/leagues/DetailsForm.js
@@ -0,0 +1,146 @@
+import React, { useState } from "react";
+import useUpdateLeague from "../../hooks/useUpdateLeague";
+
+const DetailsForm = ({ league }) => {
+  const [name, setName] = useState(league.name);
+  const [description, setDescription] = useState(league.description);
+  const [city, setCity] = useState(league.city);
+  const [state, setState] = useState(league.state);
+  const [zip, setZip] = useState(league.zip);
+  const updateLeague = useUpdateLeague();
+  return (
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        updateLeague.mutate({
+          _id: league._id,
+          name,
+          description,
+          city,
+          state,
+          zip,
+        });
+      }}
+    >
+      <div className="shadow sm:rounded-md sm:overflow-hidden">
+        <div className="bg-white py-6 px-4 space-y-6 sm:p-6">
+          <div>
+            <h3 className="text-lg leading-6 font-medium text-gray-900">
+              Details
+            </h3>
+            <p className="mt-1 text-sm text-gray-500">
+              This information will be displayed publicly.
+            </p>
+          </div>
+
+          <div className="grid grid-cols-3 gap-6">
+            <div className="col-span-3 sm:col-span-2">
+              <label
+                htmlFor="name"
+                className="block text-sm font-medium text-gray-700"
+              >
+                League Name
+              </label>
+              <div className="mt-1 rounded-md shadow-sm flex">
+                <input
+                  onChange={(e) => setName(e.target.value)}
+                  value={name}
+                  type="text"
+                  name="name"
+                  id="name"
+                  className="input_basic"
+                />
+              </div>
+            </div>
+
+            <div className="col-span-3">
+              <label
+                htmlFor="description"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Description
+              </label>
+              <div className="mt-1">
+                <textarea
+                  onChange={(e) => setDescription(e.target.value)}
+                  value={description}
+                  id="description"
+                  name="description"
+                  rows={3}
+                  className="input_basic"
+                />
+              </div>
+              <p className="mt-2 text-sm text-gray-500">
+                Brief description of your league.
+              </p>
+            </div>
+          </div>
+          <div className="col-span-6 sm:col-span-6 lg:col-span-2">
+            <label
+              htmlFor="city"
+              className="block text-sm font-medium text-gray-700"
+            >
+              City
+            </label>
+            <input
+              onChange={(e) => setCity(e.target.value)}
+              value={city}
+              type="text"
+              name="city"
+              id="city"
+              className="mt-1 input_basic"
+            />
+          </div>
+
+          <div className="col-span-6 sm:col-span-3 lg:col-span-2">
+            <label
+              htmlFor="state"
+              className="block text-sm font-medium text-gray-700"
+            >
+              State
+            </label>
+            <input
+              onChange={(e) => setState(e.target.value)}
+              value={state}
+              type="text"
+              name="state"
+              id="state"
+              className="mt-1 input_basic"
+            />
+          </div>
+
+          <div className="col-span-6 sm:col-span-3 lg:col-span-2">
+            <label
+              htmlFor="postal_code"
+              className="block text-sm font-medium text-gray-700"
+            >
+              ZIP / Postal
+            </label>
+            <input
+              onChange={(e) => setZip(e.target.value)}
+              value={zip}
+              type="text"
+              name="postal_code"
+              id="postal_code"
+              autoComplete="postal-code"
+              className="mt-1 input_basic"
+            />
+          </div>
+        </div>
+        <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
+          <button type="submit" className="btn_primary">
+            {updateLeague.isLoading
+              ? "Saving..."
+              : updateLeague.isError
+              ? "Error!"
+              : updateLeague.isSuccess
+              ? "Saved!"
+              : "Update League"}
+          </button>
+        </div>
+      </div>
+    </form>
+  );
+};
+
+export default DetailsForm;
--- src/components/leagues/ManageLeague.js
+++ src/components/leagues/ManageLeague.js
@@ -3,10 +3,24 @@ import { useParams } from "react-router-dom";
 import Loading from "../alerts/Loading";
 import Error from "../alerts/Error";
 import useLeague from "../../hooks/useLeague";
+import {
+  InformationCircleIcon,
+  LocationMarkerIcon,
+} from "@heroicons/react/outline";
+import DetailsForm from "./DetailsForm";
 
 const ManageLeague = () => {
   const { id } = useParams();
   const league = useLeague(id);
+
+  const navigation = [
+    { name: "Details", href: "#", icon: InformationCircleIcon, current: true },
+    { name: "Courses", href: "#", icon: LocationMarkerIcon, current: false },
+  ];
+
+  function classNames(...classes) {
+    return classes.filter(Boolean).join(" ");
+  }
 
   return league.isLoading ? (
     <Loading />
@@ -14,41 +28,39 @@ const ManageLeague = () => {
     <Error message={league.error.message} />
   ) : (
     <div>
-      {league.data ? (
-        <div className="mt-4">
-          <div className="flex justify-between">
-            <div>
-              <h3 className="text-lg leading-6 font-medium text-gray-900">
-                {league.data.name}
-              </h3>
-              <p className="mt-1 max-w-2xl text-sm text-gray-500">
-                {`${league.data.city}, ${league.data.state} ${
-                  league.data.zip ? league.data.zip : ""
-                }`}
-              </p>
-            </div>
-          </div>
-          <div className="mt-5 border-t border-gray-200">
-            <dl className="sm:divide-y sm:divide-gray-200">
-              <div className="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4">
-                <dt className="text-sm font-medium text-gray-500">About</dt>
-                <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                  {league.data.description}
-                </dd>
-              </div>
-              <div className="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4">
-                <dt className="text-sm font-medium text-gray-500">Organizer</dt>
-                <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                  {`${league.data.organizer.firstName} ${league.data.organizer.lastName} (${league.data.organizer.email})`}
-                </dd>
-              </div>
-              <div className="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4">
-                <dt className="text-sm font-medium text-gray-500">Members</dt>
-                <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                  {league.data.players.length}
-                </dd>
-              </div>
-            </dl>
+      {league.isIdle ? null : league.data ? (
+        <div className="mt-4 lg:grid lg:grid-cols-12 lg:gap-x-5">
+          <aside className="py-6 px-2 sm:px-6 lg:py-0 lg:px-0 lg:col-span-3">
+            <nav className="space-y-1">
+              {navigation.map((item) => (
+                <a
+                  key={item.name}
+                  href={item.href}
+                  className={classNames(
+                    item.current
+                      ? "bg-gray-50 text-indigo-700 hover:text-indigo-700 hover:bg-white"
+                      : "text-gray-900 hover:text-gray-900 hover:bg-gray-50",
+                    "group rounded-md px-3 py-2 flex items-center text-sm font-medium"
+                  )}
+                  aria-current={item.current ? "page" : undefined}
+                >
+                  <item.icon
+                    className={classNames(
+                      item.current
+                        ? "text-indigo-500 group-hover:text-indigo-500"
+                        : "text-gray-400 group-hover:text-gray-500",
+                      "flex-shrink-0 -ml-1 mr-3 h-6 w-6"
+                    )}
+                    aria-hidden="true"
+                  />
+                  <span className="truncate">{item.name}</span>
+                </a>
+              ))}
+            </nav>
+          </aside>
+
+          <div className="space-y-6 sm:px-6 lg:px-0 lg:col-span-9">
+            <DetailsForm league={league.data} />
           </div>
         </div>
       ) : (
