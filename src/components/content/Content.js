797f9e8d480c8366414948d406d2caac75050891
Lee Stratman <stratmanlc10@gmail.com>
2021-04-25 12:22:43 -0500

    Add spacing between page header and content.

--- src/components/content/Content.js
+++ src/components/content/Content.js
@@ -7,7 +7,9 @@ const Content = ({ title, children }) => {
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <h1 className="text-2xl font-semibold text-gray-900">{title}</h1>
         </div>
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">{children}</div>
+        <div className="mt-4 max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
+          {children}
+        </div>
       </div>
     </main>
   );
