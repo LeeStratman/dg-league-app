37c967a0093eb4202be45cfd8307978bdcbb81f2
Lee Stratman <stratmanlc10@gmail.com>
2021-04-25 12:23:15 -0500

    Remove secondary navigation from CreateLeague component.

--- src/pages/authenticated/CreateLeague.js
+++ src/pages/authenticated/CreateLeague.js
@@ -1,23 +1,11 @@
 import React from "react";
-import {
-  InformationCircleIcon,
-  LocationMarkerIcon,
-} from "@heroicons/react/outline";
 import Content from "../../components/content/Content";
-import WithSecondaryMenu from "../../components/content/WithSecondaryMenu";
 import CreateLeagueForm from "../../components/forms/CreateLeagueForm";
 
 const CreateLeague = () => {
-  const navigation = [
-    { name: "Details", href: "#", icon: InformationCircleIcon, current: true },
-    { name: "Courses", href: "#", icon: LocationMarkerIcon, current: false },
-  ];
-
   return (
     <Content title="Create League">
-      <WithSecondaryMenu navigation={navigation}>
-        <CreateLeagueForm />
-      </WithSecondaryMenu>
+      <CreateLeagueForm />
     </Content>
   );
 };
