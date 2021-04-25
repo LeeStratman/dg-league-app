cef4e4069f50b39fb1f53d9fc7691495a0556c3a
Lee Stratman <stratmanlc10@gmail.com>
2021-04-25 11:19:47 -0500

    Update LeagueRoutes to display info message when no league is found.

--- src/components/routes/LeagueRoutes.js
+++ src/components/routes/LeagueRoutes.js
@@ -1,12 +1,24 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
+import Info from "../alerts/Info";
 import SingleLeague from "../leagues/SingleLeague";
 
 const LeagueRoutes = () => {
   return (
     <Switch>
-      <Route path="/leagues/:leagueId" exact component={SingleLeague} />
-      <Route path="*" component={() => <div>No league selected</div>} />
+      <Route path="/leagues/:leagueId" exact>
+        <SingleLeague />
+      </Route>
+      <Route
+        path="*"
+        component={() => (
+          <Info
+            message="
+          No league selected. To find a league, please enter a name in the
+            searchbox."
+          />
+        )}
+      />
     </Switch>
   );
 };
