81b382408956ba03cc684c0c08bd12cd966c0ec7
Lee Stratman <stratmanlc10@gmail.com>
2021-04-25 16:39:58 -0500

    Remove rating from course table component.

--- src/components/courses/EditCourseTable.js
+++ src/components/courses/EditCourseTable.js
@@ -1,5 +1,6 @@
 import useRemoveCourseFromLeague from "../../hooks/mutations/useRemoveCourseFromLeague";
 import { useParams } from "react-router-dom";
+import Info from "../alerts/Info";
 
 const EditCourseTable = ({ courses }) => {
   const { leagueId } = useParams();
@@ -10,66 +11,64 @@ const EditCourseTable = ({ courses }) => {
       <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
         <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
           <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-            <table className="min-w-full divide-y divide-gray-200">
-              <thead className="bg-gray-50">
-                <tr>
-                  <th
-                    scope="col"
-                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                  >
-                    Name
-                  </th>
-                  <th
-                    scope="col"
-                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                  >
-                    Location
-                  </th>
-                  <th
-                    scope="col"
-                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                  >
-                    Rating
-                  </th>
-                  <th scope="col" className="relative px-6 py-3">
-                    <span className="sr-only">Remove</span>
-                  </th>
-                </tr>
-              </thead>
-              <tbody>
-                {courses.map((course, personIdx) => (
-                  <tr
-                    key={course._id}
-                    className={personIdx % 2 === 0 ? "bg-white" : "bg-gray-50"}
-                  >
-                    <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
-                      {course.name}
-                    </td>
-                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                      {`${course.city}, ${course.state} ${
-                        course.zip ? course.zip : ""
-                      }`}
-                    </td>
-                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                      {course.rating}
-                    </td>
-                    <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                      <button
-                        onClick={() =>
-                          removeCourse.mutate({
-                            leagueId,
-                            courseId: course.course_id,
-                          })
-                        }
-                        className="text-primary hover:text-primary-400"
-                      >
-                        Remove
-                      </button>
-                    </td>
+            {courses.length > 0 ? (
+              <table className="min-w-full divide-y divide-gray-200">
+                <thead className="bg-gray-50">
+                  <tr>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    >
+                      Name
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    >
+                      Location
+                    </th>
+
+                    <th scope="col" className="relative px-6 py-3">
+                      <span className="sr-only">Remove</span>
+                    </th>
                   </tr>
-                ))}
-              </tbody>
-            </table>
+                </thead>
+                <tbody>
+                  {courses.map((course, personIdx) => (
+                    <tr
+                      key={course._id}
+                      className={
+                        personIdx % 2 === 0 ? "bg-white" : "bg-gray-50"
+                      }
+                    >
+                      <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                        {course.name}
+                      </td>
+                      <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                        {`${course.city}, ${course.state} ${
+                          course.zip ? course.zip : ""
+                        }`}
+                      </td>
+                      <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                        <button
+                          onClick={() =>
+                            removeCourse.mutate({
+                              leagueId,
+                              courseId: course.course_id,
+                            })
+                          }
+                          className="text-primary hover:text-primary-400"
+                        >
+                          Remove
+                        </button>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            ) : (
+              <Info message="Currently no courses have been added to this league." />
+            )}
           </div>
         </div>
       </div>
