ef86583288981a7854a4b999ca55e63912e661c7
Lee Stratman <stratmanlc10@gmail.com>
2021-04-26 14:00:25 -0500

    Add event details to event details page.

--- src/pages/authenticated/MyEventDetails.js
+++ src/pages/authenticated/MyEventDetails.js
@@ -1,5 +1,36 @@
 import React from "react";
+import SubContent from "../../components/content/SubContent";
+import SubHeader from "../../components/SubHeader";
+import { displayDate } from "../../utils/date";
 
-const MyEventDetails = ({ event }) => <div>Event Details page.</div>;
+const MyEventDetails = ({ event }) => {
+  return (
+    <SubContent>
+      <SubHeader title={event.name} />
+      <div className="mt-5 border-t border-gray-200">
+        <dl className="sm:divide-y sm:divide-gray-200">
+          <div className="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4">
+            <dt className="text-sm font-medium text-gray-500">Date</dt>
+            <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+              {`${displayDate(event.date)}`}
+            </dd>
+          </div>
+          <div className="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4">
+            <dt className="text-sm font-medium text-gray-500">Description</dt>
+            <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+              {event.description}
+            </dd>
+          </div>
+          <div className="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4">
+            <dt className="text-sm font-medium text-gray-500">Layout</dt>
+            <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+              {`${event.layout.description}`}
+            </dd>
+          </div>
+        </dl>
+      </div>
+    </SubContent>
+  );
+};
 
 export default MyEventDetails;
