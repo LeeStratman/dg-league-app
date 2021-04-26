116802d4f75954c99bf380f9271e06dd1b7a185a
Lee Stratman <stratmanlc10@gmail.com>
2021-04-26 15:34:18 -0500

    Remove max width on content component.

--- src/components/content/Content.js
+++ src/components/content/Content.js
@@ -4,12 +4,10 @@ const Content = ({ title, children }) => {
   return (
     <main className="flex-1 relative z-0 overflow-y-auto focus:outline-none xl:order-last">
       <div className="py-6">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="mx-auto px-4 sm:px-6 lg:px-8">
           <h1 className="text-2xl font-semibold text-gray-900">{title}</h1>
         </div>
-        <div className="mt-4 max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
-          {children}
-        </div>
+        <div className="mt-4 mx-auto px-4 sm:px-6 md:px-8">{children}</div>
       </div>
     </main>
   );
--- src/components/content/SubContent.js
+++ src/components/content/SubContent.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const SubContent = ({ children }) => {
   return (
-    <div className="mt-4 bg-white max-w-7xl mx-auto sm:px-6 lg:px-8 shadow sm:rounded-md">
+    <div className="mt-4 bg-white mx-auto sm:px-6 lg:px-8 shadow sm:rounded-md">
       <div className="bg-white px-4 py-5 border-b border-gray-200 sm:px-6">
         {children}
       </div>
