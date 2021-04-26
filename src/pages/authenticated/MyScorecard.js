b454bd145d455ba9fd8f8c240ccf12ec229d035e
Lee Stratman <stratmanlc10@gmail.com>
2021-04-26 13:51:12 -0500

    Add ServerContent component to MyScorecard page.

--- src/pages/authenticated/MyScorecard.js
+++ src/pages/authenticated/MyScorecard.js
@@ -1,31 +1,19 @@
 import React from "react";
 import { useParams } from "react-router-dom";
-import Info from "../../components/alerts/Info";
-import Error from "../../components/alerts/Error";
 import useScorecard from "../../hooks/queries/useScorecard";
 import SubTitle from "../../components/SubTitle";
 import ScorecardForm from "../../components/forms/ScorecardForm";
+import ServerContent from "../../components/content/ServerContent";
 
 const MyScorecard = () => {
   const { eventId, scorecardId } = useParams();
   const scorecard = useScorecard(eventId, scorecardId);
 
-  return scorecard.isLoading ? (
-    <Info />
-  ) : scorecard.isError ? (
-    <Error message={scorecard.error.message} />
-  ) : (
-    <div>
-      {scorecard.isIdle ? null : scorecard.data ? (
-        <>
-          <SubTitle title="My Scorecard" />
-          <ScorecardForm scorecard={scorecard.data} />
-        </>
-      ) : (
-        <Error message="League not found." />
-      )}
-      {scorecard.isFetching ? "Updating..." : null}
-    </div>
+  return (
+    <ServerContent content={scorecard}>
+      <SubTitle title="My Scorecard" />
+      <ScorecardForm scorecard={scorecard.data} />
+    </ServerContent>
   );
 };
 
