7e03ae0dc74bbb2dc69929d4e0c7ff2ba36693da
Lee Stratman <stratmanlc10@gmail.com>
2021-04-27 16:24:13 -0500

    Add scorecard status to event tables.

--- src/components/events/EventScheduleTable.js
+++ src/components/events/EventScheduleTable.js
@@ -3,108 +3,117 @@ import { Link } from "react-router-dom";
 import { displayDate } from "../../utils/date";
 import Info from "../alerts/Info";
 import { scorecardStatus, scorecardAction } from "../../utils/user";
+import useUser from "../../hooks/queries/useUser";
+import ServerContent from "../content/ServerContent";
 
-const EventScheduleTable = ({ events = [], userId }) => {
+const EventScheduleTable = ({ events = [] }) => {
+  const user = useUser();
   return (
     <>
-      <div className="mt-4 flex flex-col">
-        <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-          <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-            {events.length > 0 ? (
-              <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-                <table className="min-w-full divide-y divide-gray-200">
-                  <thead className="bg-gray-50">
-                    <tr>
-                      <th
-                        scope="col"
-                        className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                      >
-                        Event
-                      </th>
-                      <th
-                        scope="col"
-                        className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                      >
-                        League
-                      </th>
-                      <th
-                        scope="col"
-                        className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                      >
-                        Course
-                      </th>
-                      <th
-                        scope="col"
-                        className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                      >
-                        Date
-                      </th>
-                      <th
-                        scope="col"
-                        className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                      >
-                        My Scorecard
-                      </th>
-                      <th
-                        scope="col"
-                        className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                      >
-                        Action
-                      </th>
-                      <th scope="col" className="relative px-6 py-3">
-                        <span className="sr-only">View</span>
-                      </th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {events.map((event, eventIdx) => (
-                      <tr
-                        key={event._id}
-                        className={
-                          eventIdx % 2 === 0 ? "bg-white" : "bg-gray-50"
-                        }
-                      >
-                        <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
-                          {event.name}
-                        </td>
-                        <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
-                          {event.leagueId.name}
-                        </td>
-                        <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
-                          {event.layout?.course?.name
-                            ? event.layout.course.name
-                            : ""}
-                        </td>
-                        <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                          {displayDate(event.date)}
-                        </td>
-                        <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                          {scorecardStatus(event, userId)}
-                        </td>
-                        <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                          {scorecardAction(event, userId)}
-                        </td>
-                        <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                          <Link
-                            to={`/my-leagues/${event.leagueId}/events/${event._id}`}
-                            className="text-primary hover:text-primary-400"
+      <ServerContent content={user}>
+        <div className="mt-4 flex flex-col">
+          <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+            <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+              {events.length > 0 ? (
+                <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                  <table className="min-w-full divide-y divide-gray-200">
+                    <thead className="bg-gray-50">
+                      <tr>
+                        <th
+                          scope="col"
+                          className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                        >
+                          Event
+                        </th>
+                        {events[0].leagueId.name && (
+                          <th
+                            scope="col"
+                            className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
                           >
-                            View
-                          </Link>
-                        </td>
+                            League
+                          </th>
+                        )}
+                        <th
+                          scope="col"
+                          className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                        >
+                          Course
+                        </th>
+                        <th
+                          scope="col"
+                          className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                        >
+                          Date
+                        </th>
+                        <th
+                          scope="col"
+                          className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                        >
+                          My Scorecard
+                        </th>
+                        <th
+                          scope="col"
+                          className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                        >
+                          Action
+                        </th>
+                        <th scope="col" className="relative px-6 py-3">
+                          <span className="sr-only">View</span>
+                        </th>
                       </tr>
-                    ))}
-                  </tbody>
-                </table>
-              </div>
-            ) : (
-              <div className="mt-2">
-                <Info message="There are currently no events." />
-              </div>
-            )}
+                    </thead>
+                    <tbody>
+                      {events.map((event, eventIdx) => (
+                        <tr
+                          key={event._id}
+                          className={
+                            eventIdx % 2 === 0 ? "bg-white" : "bg-gray-50"
+                          }
+                        >
+                          <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                            {event.name}
+                          </td>
+                          {event.leagueId.name && (
+                            <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                              {event.leagueId.name}
+                            </td>
+                          )}
+                          <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                            {event.layout?.course?.name
+                              ? event.layout.course.name
+                              : ""}
+                          </td>
+                          <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                            {displayDate(event.date)}
+                          </td>
+                          <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                            {scorecardStatus(event, user.data?._id)}
+                          </td>
+                          <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                            {scorecardAction(event, user.data?._id)}
+                          </td>
+                          <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                            <Link
+                              to={`/my-leagues/${event.leagueId}/events/${event._id}`}
+                              className="text-primary hover:text-primary-400"
+                            >
+                              View Event
+                            </Link>
+                          </td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>
+                </div>
+              ) : (
+                <div className="mt-2">
+                  <Info message="There are currently no events." />
+                </div>
+              )}
+            </div>
           </div>
         </div>
-      </div>
+      </ServerContent>
     </>
   );
 };
--- src/pages/authenticated/MyLeagueEvents.js
+++ src/pages/authenticated/MyLeagueEvents.js
@@ -4,6 +4,7 @@ import useLeagueEvents from "../../hooks/queries/useLeagueEvents";
 import SubContent from "../../components/content/SubContent";
 import SubHeader from "../../components/SubHeader";
 import ServerContent from "../../components/content/ServerContent";
+import EventScheduleTable from "../../components/events/EventScheduleTable";
 
 const MyLeagueEvents = ({ leagueId }) => {
   const events = useLeagueEvents(leagueId);
@@ -12,7 +13,8 @@ const MyLeagueEvents = ({ leagueId }) => {
     <SubContent>
       <SubHeader title="Events" />
       <ServerContent content={events}>
-        <LeagueEventList leagueId={leagueId} events={events.data} />
+        <EventScheduleTable events={events.data} />
+        {/* <LeagueEventList leagueId={leagueId} events={events.data} /> */}
       </ServerContent>
     </SubContent>
   );
