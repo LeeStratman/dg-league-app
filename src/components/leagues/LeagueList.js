fc049091b248e1eade197710426cb15666ce9122
Lee Stratman <stratmanlc10@gmail.com>
2021-04-27 15:48:45 -0500

    Add error message when searching for leagues.

--- src/components/leagues/LeagueList.js
+++ src/components/leagues/LeagueList.js
@@ -1,15 +1,22 @@
 import LeagueItem from "./LeagueItem";
 import useLeagueSearch from "../../hooks/queries/useLeagueSearch";
 import ServerContent from "../content/ServerContent";
+import Error from "../alerts/Error";
 
 const LeagueList = ({ name }) => {
   const leagues = useLeagueSearch(name);
 
   return (
     <ServerContent content={leagues}>
-      {leagues.data?.map((league) => (
-        <LeagueItem key={league._id} league={league} />
-      ))}
+      {leagues.data?.length > 0 ? (
+        leagues.data?.map((league) => (
+          <LeagueItem key={league._id} league={league} />
+        ))
+      ) : (
+        <Error
+          message={`No leagues were found with the name containing '${name}'`}
+        />
+      )}
     </ServerContent>
   );
 };
